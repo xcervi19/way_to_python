f4d4a1c4925147b122186e1f619ba819bc50347b
Karel Cervicek <cervicekkarel@gmail.com>
2025-07-07 15:27:16 +0200

    lesson1

--- lesson1.py
+++ lesson1.py
@@ -1,6 +1,7 @@
-Python neni sysntax python je mindset
+python neni syntax ale minset
 
 1. None neni False
-2. is vs. =
-3. default mutable args KILL you 
-4.data stream yield > return
+2. is vs. ==
+3. mutable default args KILL YOU
+    def some_append(x, lst=None):
+4. return vs yield
